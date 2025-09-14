8e46bf18fcfd403acfb2f00f5bd1ff180c219d0d
AsifIslamBisal <bisalasig@gmail.com>
2025-09-14 19:06:28 +0600

    error page updated

--- src/ErrorPage/ErrorPage.jsx
+++ src/ErrorPage/ErrorPage.jsx
@@ -3,11 +3,11 @@ import Lottie from "lottie-react";
 import errorAnimation from '../assets/lottie/error.json';
 const ErrorPage = () => {
     return (
-        <div className="error-page">
+        <div className="error-page flex items-center justify-center">
         <h1>404 Not Found</h1>
         <p>Sorry, the page you're looking for doesn't exist.</p>
-        <a href="/">Go Back to Home</a>
-        <div>
+        <a className='text-blue-600' href="/">Go Back to Home</a>
+        <div className='w-96'>
           <Lottie animationData={errorAnimation}></Lottie>
         </div>
       </div>
